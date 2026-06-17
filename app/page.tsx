d136201735ab373f8675156f64a2ac2e171c7fae
Joshua Gretton <jb.gretton@googlemail.com>
2026-06-17 09:13:49 +0100

    rotation implemented
    Player base positions and rotations working. MB changes to Lib backcourt
    and back to MB when front court.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+export type PositionCoordinate = {
+	position: number;
+	x: number;
+	y: number;
+};
+export type PlayerPosition = {
+	positionName: string;
+	position: number;
+};
+
 export default function Home() {
-	const positions = [1, 2, 3, 4, 5, 6];
-
-	const playerBasePosition = [
-		{ position: "MB", x: 45, y: 20 },
-		{ position: "O1", x: 75, y: 20 },
-		{ position: "S", x: 75, y: 70 },
-		{ position: "O2", x: 15, y: 70 },
-		{ position: "OP", x: 15, y: 20 },
-		{ position: "L", x: 45, y: 70 },
-	];
-
 	/* 
     2 array approach
     1 array with base x and y positions for each position, 1-6
@@ -17,6 +20,60 @@ export default function Home() {
 
     When we rotate the players, we just change the player position number.
 */
+
+	const positionCoordinates: PositionCoordinate[] = [
+		{ position: 3, x: 45, y: 20 },
+		{ position: 2, x: 75, y: 20 },
+		{ position: 1, x: 75, y: 70 },
+		{ position: 5, x: 15, y: 70 },
+		{ position: 4, x: 15, y: 20 },
+		{ position: 6, x: 45, y: 70 },
+	];
+
+	const initialPlayerPositions: PlayerPosition[] = [
+		{ positionName: "S", position: 2 },
+		{ positionName: "L", position: 1 },
+		{ positionName: "OH2", position: 6 },
+		{ positionName: "OP", position: 5 },
+		{ positionName: "MB", position: 4 },
+		{ positionName: "OH1", position: 3 },
+	];
+
+	const [playerPositions, setPlayerPositions] = useState<PlayerPosition[]>(
+		initialPlayerPositions,
+	);
+
+	/*
+        This function needs to change...
+        we need to check if a MB goes backcourt, then it becomes L, if L goes front court then it becomes MB
+
+        take position name and location
+        if MB and 1 then L if L and 4 then MB
+    */
+
+	const rotatePositions = () => {
+		setPlayerPositions((prev) => {
+			const newArray = prev.map((player) => {
+				const newPosition = player.position === 1 ? 6 : player.position - 1;
+				let newPositionName = player.positionName;
+
+				// Change mb to lib when moving backcourt.
+				// change lib to mb when moving front court.
+				if (player.positionName === "L" && newPosition === 4) {
+					newPositionName = "MB";
+				} else if (player.positionName === "MB" && newPosition === 1) {
+					newPositionName = "L";
+				}
+
+				return {
+					positionName: newPositionName,
+					position: newPosition,
+				};
+			});
+
+			return newArray;
+		});
+	};
 
 	const PlayerIcon = ({ pos, x, y }: { pos: string; x: number; y: number }) => {
 		return (
@@ -33,12 +90,12 @@ export default function Home() {
 		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
 			<main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32  sm:items-start">
 				<h1>Rotation Maker</h1>
-
+				<button onClick={rotatePositions}>Rotate</button>
 				<div className="max-w-2xl mx-auto w-full  mt-20">
 					{/* outer */}
 					<div className="aspect-square bg-teal-700/30 relative">
 						{/* net */}
-						<div className="top-0 w-full h-7 bg-amber-600 z-50 -inset-x-10 absolute right-0 left-0 rounded-lg" />
+						<div className="top-0 w-full h-7 bg-mauve-800 z-50 -inset-x-10 absolute right-0 left-0 rounded-lg" />
 						{/* 3m extended */}
 						{/* court */}
 						<div className="inset-x-[7%] aspect-square inset-y-0 bg-orange-500  absolute z-10 rounded-b-md border-3 border-white">
@@ -48,18 +105,19 @@ export default function Home() {
 							<div className="absolute inset-[-7%] top-[33%] h-1 bg-[repeating-linear-gradient(to_right,white_0_5px,transparent_5px_10px)]" />
 
 							<div className="size-full grid grid-cols-3 grid-rows-2 mt-6 relative">
-								{playerBasePosition.map((player) => (
-									<div
-										key={player.position}
-										className="size-full border border-dashed border-white/50 hover:bg-orange-800/30"
-									>
-										<PlayerIcon
-											pos={player.position}
-											x={player.x}
-											y={player.y}
-										/>
-									</div>
-								))}
+								{playerPositions.map((player) => {
+									const { x, y } = positionCoordinates.find(
+										(ps) => ps.position === player.position,
+									) ?? { x: 0, y: 0 };
+									return (
+										<div
+											key={player.position}
+											className="size-full border border-dashed border-white/50 hover:bg-orange-800/30"
+										>
+											<PlayerIcon pos={player.positionName} x={x} y={y} />
+										</div>
+									);
+								})}
 							</div>
 						</div>
 					</div>
